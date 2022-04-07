e7b275652a254294c20961ff6bb143697a54082d
XiNiHa <me@xiniha.dev>
2022-04-07 15:49:45 +0900

    Init project

---
+++ packages/docs/src/components/RightSidebar/TableOfContents.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'solid-js'
+import 'solid-js/web'
+
+const TableOfContents: Component<{ headers: any[] }> = ({ headers = [] }) => {
+  return (
+    <>
+      <h2 class="heading">On this page</h2>
+      <ul>
+        <li class={`header-link depth-2`.trim()}>
+          <a href="#overview">Overview</a>
+        </li>
+        {headers
+          .filter(({ depth }) => depth > 1 && depth < 4)
+          .map((header) => (
+            <li class={`header-link depth-${header.depth}`.trim()}>
+              <a href={`#${header.slug}`}>{header.text}</a>
+            </li>
+          ))}
+      </ul>
+    </>
+  )
+}
+
+export default TableOfContents
