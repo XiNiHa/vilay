d6db0f8e4e1615233f22660d2fe956d35df336bf
XiNiHa <me@xiniha.dev>
2022-05-06 14:24:21 +0900

    Add sitemap for docs

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 import solid from '@astrojs/solid-js'
-import react from '@astrojs/react';
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://vilay.xiniha.dev',
-	integrations: [
-		solid(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-});
+  site: 'https://vilay.xiniha.dev',
+  integrations: [
+    solid(),
+    react(), // Enable React for the Algolia search component.
+    sitemap(),
+  ],
+})
