f5462e161fe4d001263a07a2f87424de609bfdeb
XiNiHa <me@xiniha.dev>
2022-06-17 15:46:23 +0900

    Use JSON format for Relay config

--- packages/create/templates/typescript/relay.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  src: '.',
-  schema: './schema.graphql',
-  excludes: ['**/dist/**', '**/node_modules/**', '**/__generated__/**'],
-  language: 'typescript',
-  eagerEsModules: true,
-}
