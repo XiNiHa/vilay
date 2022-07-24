13083d1367ba098ddbae923658a146368cddb1b3
XiNiHa <me@xiniha.dev>
2022-07-25 05:29:17 +0900

    Expose <VilayApp /> component for convenience

--- packages/example/renderer/PageShell.tsx
+++ packages/example/renderer/PageShell.tsx
@@ -1,38 +1,24 @@
 import React from 'react'
-import { RelayEnvironmentProvider, type Environment } from 'react-relay'
-import { useRouteManager } from 'vilay'
-
-import type { PageContext, RouteManager } from 'vilay'
-
-import { PageContextProvider } from './usePageContext'
-
-export interface PageShellProps {
-  pageContext: PageContext
-  relayEnvironment: Environment
-  routeManager: RouteManager
-}
+import { useRouteManager, VilayApp, type PageShellProps } from 'vilay'
 
 // Custom Page root component
 export const PageShell: React.FC<PageShellProps> = ({
-  pageContext,
   relayEnvironment,
   routeManager,
 }) => {
-  const PageLayout =
-    pageContext.exports?.PageLayout ??
-    pageContext.exports?.pageLayout ??
-    Passthrough
-  const [CurrentPage, queryRef, routeTransitioning] =
+  const [CurrentPage, pageContext, queryRef, routeTransitioning] =
     useRouteManager(routeManager)
+  const PageLayout =
+    pageContext?.exports?.PageLayout ??
+    pageContext?.exports?.pageLayout ??
+    Passthrough
   return (
     <React.StrictMode>
-      <RelayEnvironmentProvider environment={relayEnvironment}>
-        <PageContextProvider pageContext={pageContext}>
-          <PageLayout routeTransitioning={routeTransitioning}>
-            {CurrentPage && <CurrentPage queryRef={queryRef} />}
-          </PageLayout>
-        </PageContextProvider>
-      </RelayEnvironmentProvider>
+      <VilayApp relayEnvironment={relayEnvironment} pageContext={pageContext}>
+        <PageLayout routeTransitioning={routeTransitioning}>
+          {CurrentPage && <CurrentPage queryRef={queryRef} />}
+        </PageLayout>
+      </VilayApp>
     </React.StrictMode>
   )
 }
--- packages/vilay/index.ts
+++ packages/vilay/index.ts
@@ -3,6 +3,12 @@ export { RouteManager, useRouteManager } from './renderer/routeManager'
 
 export { defineVilay } from './types.js'
 
+export {
+  VilayApp,
+  type PageShellProps,
+  type VilayAppProps,
+} from './renderer/PageShell'
+
 export type {
   PageContext,
   PageLayoutProps,
